318cc0c5b15b64f035f19782aa85472bbb790a95
Eddy Dai <dyd23@qq.com>
2020-09-28 11:20:42 +0800

    Update fill.sh

--- fill.sh
+++ fill.sh
@@ -1,5 +1,5 @@
 #！/bin/bash
 source /your/path/to/conda/bin/activate
-# Xvfb :99 -ac -screen 0 1280x1024x24 & #仅需要启动一次
+Xvfb :99 -ac -screen 0 1280x1024x24 & 
 export DISPLAY=:99
 python /your/path/to/fill.py
